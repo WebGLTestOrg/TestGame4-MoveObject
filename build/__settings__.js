3f1a3007cb48acb6f3dcb88099d68644b8eaaa5e
Alex <voronko20172017@gmail.com>
2025-05-16 11:07:21 +0300

    Init

---
+++ build/__settings__.js
@@ -0,0 +1,22 @@
+window.ASSET_PREFIX = "./build/";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "2226444.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'deviceTypes': [`webgl2`, `webgl1`],
+    'powerPreference': "high-performance"
+};
+window.SCRIPTS = [ 225518055, 225518045, 225518048, 225518061, 225518064, 226410822, 226410823, 226410824, 226411650, 226411672, 226412016, 226439737, 226447556, 226447911, 226447912, 228809471 ];
+window.CONFIG_FILENAME = window.ASSET_PREFIX + "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/225518042/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/225518043/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/225518041/1/ammo.js', 'preload' : true},
+];
